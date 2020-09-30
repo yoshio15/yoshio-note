2d2e830b4d5343ac9992cce9fc2cd78ae0b6d38c
YoshioKondo <yoshio15310@gmail.com>
2020-09-30 18:50:34 +0900

    set title font

--- src/utils/typography.js
+++ src/utils/typography.js
@@ -20,7 +20,7 @@ GitHubTheme.overrideThemeStyles = () => {
     h1: {
       fontWeight: 800,
       lineHeight: 1.2,
-      fontFamily: 'Catamaran',
+      fontFamily: 'Hannotate SC' // Title Font
     },
 
     h2: {
@@ -28,7 +28,7 @@ GitHubTheme.overrideThemeStyles = () => {
       lineHeight: 1.2,
       marginTop: '56px',
       marginBottom: '20px',
-      fontFamily: 'Catamaran',
+      fontFamily: 'Hannotate SC', // SubTitle Font
     },
 
     ul: {
