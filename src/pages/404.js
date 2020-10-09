c75003bbb7cf2f375dadcee3dc5c26a92e9310f9
YoshioKondo <yoshio15310@gmail.com>
2020-10-09 23:05:37 +0900

    custom 404 page

--- src/pages/404.js
+++ src/pages/404.js
@@ -3,6 +3,7 @@ import { graphql } from 'gatsby'
 
 import { Layout } from '../layout'
 import { Head } from '../components/head'
+import NotFound from '../../static/not_found.png'
 
 class NotFoundPage extends React.Component {
   render() {
@@ -12,8 +13,7 @@ class NotFoundPage extends React.Component {
     return (
       <Layout location={this.props.location} title={siteTitle}>
         <Head title="404: Not Found" />
-        <h1>Not Found</h1>
-        <p>You just hit a route that doesn&#39;t exist... the sadness.</p>
+        <img src={NotFound} />
       </Layout>
     )
   }
